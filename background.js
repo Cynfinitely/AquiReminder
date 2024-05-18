622eb28562d2b1a85dec890c725eba2f18feca2a
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-05-18 23:59:49 +0300

    alarm logic created

--- background.js
+++ background.js
@@ -1,17 +1,9 @@
-chrome.runtime.onInstalled.addListener(function () {
-  // Create an alarm that fires every 10 seconds
-  chrome.alarms.create("Water Reminder", { periodInMinutes: 1 / 6 });
-});
-
-chrome.alarms.onAlarm.addListener(function (alarm) {
-  if (alarm.name === "Water Reminder") {
-    console.log("FIRED");
-    // Show a notification when the alarm fires
-    chrome.notifications.create("Water Reminder", {
-      type: "basic",
-      iconUrl: chrome.runtime.getURL("icon.png"),
-      title: "Water Reminder",
-      message: "Time to drink water!",
-    });
-  }
+chrome.alarms.onAlarm.addListener((alarm) => {
+  //alert('Alret');
+  chrome.windows.create({
+    width : 200,
+    height : 100,
+    type : 'popup',
+    url : 'alert.html'
+  });
 });
