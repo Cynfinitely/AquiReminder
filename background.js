a492d29258ff391c85495d0354a0f97e3401968c
Celal Yasin Nari <celalyasinnari@gmail.com>
2024-05-19 22:24:28 +0300

    finished.

--- background.js
+++ background.js
@@ -1,9 +1,21 @@
+// background.js
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "setAlarm") {
+    let dt = new Date();
+    dt.setMinutes(dt.getMinutes() + parseInt(request.sec)); // change setSeconds to setMinutes
+    chrome.alarms.create(request.alarm_id, { when: dt.getTime() });
+    console.log(`Alarm set for ${request.sec} minutes from now.`);
+  } else if (request.action === "clearAlarm") {
+    chrome.alarms.clear(request.alarm_id);
+  }
+});
+
 chrome.alarms.onAlarm.addListener((alarm) => {
-  //alert('Alret');
   chrome.windows.create({
-    width : 200,
-    height : 100,
-    type : 'popup',
-    url : 'alert.html'
+    width: 400,
+    height: 400,
+    type: "popup",
+    url: "alert.html",
   });
+  chrome.storage.local.set({ alarmSet: false, remainingMinutes: null });
 });
--- popup.js
+++ popup.js
@@ -1,10 +1,41 @@
+// myscript.js
 const alarm_id = "alarm_001";
+
+// Load state from storage when the script starts
+chrome.storage.local.get(["alarmSet", "remainingMinutes"], function (result) {
+  if (result.alarmSet) {
+    document.getElementById("setReminder").style.display = "none";
+    document.getElementById("remainingTime").style.display = "block";
+    document.getElementById("time").innerText =
+      result.remainingMinutes + " minutes";
+  }
+});
+
 document.getElementById("id_Set").onclick = () => {
-  let dt = new Date();
-  let sec = document.getElementById("id_Sec").value;
-  dt.setSeconds(dt.getSeconds() + parseInt(sec));
-  chrome.alarms.create(alarm_id, { when: dt.getTime() });
+  let min = document.getElementById("id_Sec").value;
+  let sec = min ; 
+  chrome.runtime.sendMessage({
+    action: "setAlarm",
+    alarm_id: alarm_id,
+    sec: sec,
+  });
+
+  // Hide setReminder div and show remainingTime div
+  document.getElementById("setReminder").style.display = "none";
+  document.getElementById("remainingTime").style.display = "block";
+  document.getElementById("time").innerText = min + " minutes";
+
+  // Save state to storage
+  chrome.storage.local.set({ alarmSet: true, remainingMinutes: min });
 };
+
 document.getElementById("id_Clear").onclick = () => {
-  chrome.alarms.clear(alarm_id);
+  chrome.runtime.sendMessage({ action: "clearAlarm", alarm_id: alarm_id });
+
+  // Show setReminder div and hide remainingTime div
+  document.getElementById("setReminder").style.display = "block";
+  document.getElementById("remainingTime").style.display = "none";
+
+  // Clear state from storage
+  chrome.storage.local.set({ alarmSet: false, remainingMinutes: null });
 };
